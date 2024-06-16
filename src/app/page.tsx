418cb21e1fe1a5278b6c30ce070a6ef61b7dfa1a
Chinonso Ude <chinonso25@gmail.com>
2024-06-16 09:45:06 +0100

    PWORD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { useEffect, useMemo, useState } from 'react'
 import { exportPdf } from './helpers'
 import { SingleDatepicker } from 'chakra-dayzed-datepicker'
 
-const PASSWORD = 'GOSPEL'
+const PASSWORD = 'GOSPELMARK'
 
 export default function Home() {
   const [classSelected, setClassSelected] = useState<ClassRooms>(ClassRooms.INSPIRATION)
